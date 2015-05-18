da6ac2165fe2d1a34b8bc535d0d6026d8e5b9da4
Timur Morozov <radist@desant.biz>
2015-05-18 22:54:40 +0300

    list template for brooklyn

--- template-hotels-list.php
+++ template-hotels-list.php
@@ -2,25 +2,28 @@
 /**
  * Template Name: Hotels List
  */
-?>
 
-<?php
-get_header('search-hotel'); ?>
+$ut_page_skin = get_post_meta( $post->ID , 'ut_section_skin' , true);
+$ut_page_class = get_post_meta( $post->ID , 'ut_section_class' , true);
 
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+get_header(); ?>
+    
+<div class="grid-container">
+  
+  <?php $grid = is_active_sidebar('page-widget-area') ? 'grid-75 tablet-grid-75 mobile-grid-100' : 'grid-100 tablet-grid-100 mobile-grid-100'; ?>
+    
+  <div id="primary" class="grid-parent <?php echo $grid; ?> <?php echo $ut_page_skin; ?> <?php echo $ut_page_class; ?>">
+    
+    <ul class="sidebar widget-area">
+      <?php dynamic_sidebar( 'tim_exp_api_list_one' ); ?>
+    </ul>
 
-      <h1>Hello Hotels List Template</h1>
+    <ul class="sidebar widget-area">
+      <?php dynamic_sidebar( 'tim_exp_api_list_two' ); ?>
+    </ul>
 
-      <div class="widget-area">
-        <?php dynamic_sidebar( 'tim_exp_api_list_one' ); ?>
-      </div>
-
-      <div class="widget-area">
-        <?php dynamic_sidebar( 'tim_exp_api_list_two' ); ?>
-      </div>
-
-    </main><!-- .site-main -->
-  </div><!-- .content-area -->
-
-<?php get_footer('search-hotel'); ?>
+  </div><!-- close #primary -->
+</div><!-- close grid-container -->
+    
+        
+<?php get_footer(); ?>
